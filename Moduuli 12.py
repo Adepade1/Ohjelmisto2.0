cd8880a82ee21a23e677792e28ecc49b3db8b42f
Adepade1 <attemah@metropolia.fi>
2023-03-24 11:05:45 +0200

    done

---
+++ Moduuli 12.py
@@ -0,0 +1,31 @@
+import requests
+
+#Tehtävä 1
+
+pyyntö = "https://api.chucknorris.io/jokes/random"
+
+try:
+    vastaus = requests.get(pyyntö)
+    if vastaus.status_code == 200:
+        json_vastaus = vastaus.json()
+        print(f"{json_vastaus['value']}")
+
+except requests.exceptions.RequestException as e:
+    print ("Hakua ei voitu suorittaa.")
+
+
+#Tehtävä 2
+
+hakusana = input("Hakusana")
+pyyntöX = "https://api.openweathermap.org/data/2.5/weather?q=" + hakusana + \
+          "&appid=8ef406a6dbb491658bcb473f6242aa69&units=metric&lang=fi"
+try:
+    vastausX = requests.get(pyyntöX)
+    if vastausX.status_code == 200:
+        json_vastaus = vastausX.json()
+
+    print(f"keli: {json_vastaus['weather'][0] ['description']}")
+    print(f"Lämpötila: {json_vastaus['main']['temp']}C°")
+
+except requests.exceptions.RequestException as e:
+    print ("Hakua ei voitu suorittaa.")
